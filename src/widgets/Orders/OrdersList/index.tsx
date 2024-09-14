9aae04a41bf7c06315592d81da251d6643d46649
cfifeg1@gmail.com <cfifeg1@gmail.com>
2024-09-14 18:37:48 +0300

    microfix

--- src/widgets/Orders/OrdersList/index.tsx
+++ src/widgets/Orders/OrdersList/index.tsx
@@ -17,37 +17,36 @@ export const OrdersList = (): ReactElement => {
     const { currentFilter, currentPriceSort } = useOrderStateSelector();
     const { updateOrderState } = useAppContext();
     const { data, isError, isLoading } = useOrders(currentFilter, currentPriceSort);
-    
-    const [params] = useSearchParams()
 
-    const adId = params.get(QUERY_PARAMS.AD_ID) 
+    const [params] = useSearchParams();
 
-    const navigate = useNavigate()
-    
+    const adId = params.get(QUERY_PARAMS.AD_ID);
+
+    const navigate = useNavigate();
+
     useEffect(() => {
         return () => {
             updateOrderState({ adId: null, currentFilter: null, currentPriceSort: null });
         };
-    }, [updateOrderState]);
-    
+    }, []);
+
     if (isError) return <>Ошибка при загрузке данных</>;
 
     if (isLoading) return <Loader />;
-    
 
     if (adId && data) {
         const filteredData = data.filter(order => order.items.some(item => item.id === adId));
-        
+
         const backNavigate = () => {
-            navigate(-1)
-        }
-        
+            navigate(-1);
+        };
+
         return (
             <div className={styles.wrapper}>
                 {size(filteredData) === 0 ? (
                     <>
                         <span>По данному товару заказы отсутствуют</span>{' '}
-                        <Button onClick={backNavigate} label='Вернуться назад' variant='empty'/>
+                        <Button onClick={backNavigate} label='Вернуться назад' variant='empty' />
                     </>
                 ) : (
                     map(filteredData, item => <OrderCard {...item} />)
@@ -55,7 +54,7 @@ export const OrdersList = (): ReactElement => {
             </div>
         );
     }
-    
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.filters}>
